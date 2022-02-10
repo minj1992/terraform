7f6acba281daa4a74371bab6435e8dd9872bd583
minj1992 <57328135+minj1992@users.noreply.github.com>
2022-02-10 03:46:17 -0800

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAX736SA5VYEDCD3PI"
-  secret_key = "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"
+  access_key = "xxxxxxxxxxxxxxxxxxx"
+  secret_key = "xxxxxxxxxxxxxxxxxxxx"
 }
